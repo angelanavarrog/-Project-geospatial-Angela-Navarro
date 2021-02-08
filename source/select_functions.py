c139f75d6d729d283caa245cc813d79f8def9a36
Ángela <angela.navarro.gonzalez@gmail.com>
2021-02-08 23:51:49 +0100

    source/

--- source/select_functions.py
+++ source/select_functions.py
@@ -19,15 +19,16 @@ from cartoframes.viz import Map, Layer, popup_element
 client = MongoClient()
 db = client.companies
 
-def results1(filt1, projects1):
+def location_suggestions(filt1, projects1):
     ''' It takes the company information filtered by our defined criteria.
     It returns a Dataframe with company name and the office location (city, lat, long)'''
 
     filt1 = {"number_of_employees": {"$eq":90},"offices.city": {'$eq': "New York"}}
     project1 = {"_id":0,"name":1,"offices.city": 1,"number_of_employees": 1,"offices.latitude":1,"offices.longitude":1}
     results1 = db.offices.find(filt1,project1).sort("number_of_employees",1)
-    return results1
-
+    office_location = list(results1)
+    final_location = pd.DataFrame.from_dict(office_location)
+    return final_location
 
 def office_coordinates (office_data):
     ''' It takes the company company coooordinates and returns a Dataframe with company name, office location and criteria'''
